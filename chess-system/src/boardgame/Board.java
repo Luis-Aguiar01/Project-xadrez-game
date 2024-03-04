37ea1cb021facee447e4f4c99783ac334494bc18
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2024-03-04 18:33:39 -0300

    Add exceptions

--- chess-system/src/boardgame/Board.java
+++ chess-system/src/boardgame/Board.java
@@ -1,9 +1,9 @@
 package boardgame;
 
 public class Board {
-    private int rows;
-    private int columns;
-    private Piece[][] pieces;
+    private final int rows;
+    private final int columns;
+    private final Piece[][] pieces;
 
     public Board(int rows, int columns) {
         this.columns = columns;
@@ -15,28 +15,44 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
-
     public Piece getPiece(int row, int column) {
+        if (!positionExists(row, column)) {
+            throw new BoardException("Position not on the board.");
+        }
         return pieces[row][column];
     }
 
     public Piece getPiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board.");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position) {
+        if (thereIsAPiece(position)) {
+            throw new BoardException("There is already a piece on position " + position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    private boolean positionExists(int row, int column) {
+        return ( row >= 0 && row < rows ) && ( column >= 0 && column < columns );
+    }
+
+    public boolean positionExists(Position position) {
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board.");
+        }
+        return getPiece(position) != null;
     }
 }
