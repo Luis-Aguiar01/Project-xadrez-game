d7f3fdab67d972c5fa1a68ac5c30e6f5dc095374
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2024-03-04 16:09:29 -0300

    Create Position class

---
+++ chess-system/src/applicattion/Program.java
@@ -0,0 +1,13 @@
+package applicattion;
+
+import boardgame.Position;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Position p = new Position(1, 2);
+        System.out.println(p);
+
+    }
+}
